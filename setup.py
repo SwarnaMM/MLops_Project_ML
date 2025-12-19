46c89db6ad4ef13000f2821c28e5d0cff3820ac7
SwarnaMM <mswarna304@gmail.com>
2025-12-19 11:34:05 -0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="MLops_Project_ML",
+    version="0.0.0",
+    author="Swarna",
+    swarna_email="mswarna_304@gmail.com",
+    packages=find_packages()
+
+)
